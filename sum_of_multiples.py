de30a4209e621b05724fa53f0b4e7cfde90ebda9
Dominik Böhler <289612+dominikb1888@users.noreply.github.com>
2022-04-20 21:19:30 +0200

    first attempt with loop

--- sum_of_multiples.py
+++ sum_of_multiples.py
@@ -1,2 +1,7 @@
 def sum_of_multiples(limit, multiples):
-    pass
+    result_set = set()
+    for i in range(1, limit):
+        for factor in multiples:
+            if factor != 0 and i % factor == 0:
+                result_set.add(i)
+    return sum(result_set)
