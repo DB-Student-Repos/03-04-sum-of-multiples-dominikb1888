a7f56956d037aed189a5b55fd6e495874c371f12
Dominik Böhler <289612+dominikb1888@users.noreply.github.com>
2022-04-20 21:22:21 +0200

    second attempt with set comprehension as one liner

--- sum_of_multiples.py
+++ sum_of_multiples.py
@@ -1,9 +1,2 @@
 def sum_of_multiples(limit, multiples):
-    return sum(
-        {
-            i
-            for i in range(1, limit)
-            for factor in multiples
-            if factor != 0 and i % factor == 0
-        }
-    )
+    return sum({i for i in range(1, limit) for f in multiples if f != 0 and i % f == 0})
