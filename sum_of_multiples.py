d61ca72b0fcd4591caa3084dcc30b1656a1be0bc
Dominik Böhler <289612+dominikb1888@users.noreply.github.com>
2022-04-20 21:28:18 +0200

    third attempt using the factors and a sliding range

--- sum_of_multiples.py
+++ sum_of_multiples.py
@@ -1,2 +1,2 @@
 def sum_of_multiples(limit, multiples):
-    return sum({i for i in range(1, limit) for f in multiples if f != 0 and i % f == 0})
+    return sum({i for f in multiples if f for i in range(f, limit, f)})
